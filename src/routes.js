d4cee0a10a83331aee4c42b29259bdd7675273e0
Diego Azevedo <dapptn@hotmail.com>
2019-08-21 22:10:16 +1000

    Eslint, prettier

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "It is ok!" });
-});
+routes.get('/', (req, res) => res.json({ message: 'It is ok!' }));
 
 export default routes;
